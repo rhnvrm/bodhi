c6eb2f3f5236e25df8ff8acb8e9fca927e8dd7fd
Rohan Verma <hello@rohanverma.net>
2020-06-05 03:51:35 +0530

    fix: use baseurl in search.js

--- static/js/search.js
+++ static/js/search.js
@@ -28,7 +28,7 @@ if(searchQuery){
 
 
 function executeSearch(searchQuery){
-  $.getJSON( "/index.json", function( data ) {
+  $.getJSON( baseURL + "index.json", function( data ) {
     var pages = data;
     var fuse = new Fuse(pages, fuseOptions);
     var result = fuse.search(searchQuery);
