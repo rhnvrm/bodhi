c85ba0eb040810586e66350b9ff390bc20c50244
Rohan Verma <hello@rohanverma.net>
2020-09-19 01:29:27 +0530

    feat: Add dark mode toggle to menu
    This commit adds support for dark-mode.
    
    It changes the css to use vars which are toggled
    based on data stored in local storage. The state can be modified
    by the buttons in the menu.

--- static/js/main.js
+++ static/js/main.js
@@ -2,3 +2,20 @@ let element = document.getElementById("main");
 if (document.body.clientWidth < 632) {
     element.scrollIntoView({behavior: "smooth"});
 }
+
+
+// Set the theme
+// Ref: https://livecodestream.dev/post/2020-08-06-a-better-approach-to-dark-mode-on-your-website/
+// Capture the current theme from local storage and adjust the page to use the current theme.
+const htmlEl = document.getElementsByTagName('html')[0];
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    htmlEl.dataset.theme = currentTheme;
+}
+
+// When the user changes the theme, we need to save the new value on local storage
+const toggleTheme = (theme) => {
+    htmlEl.dataset.theme = theme;
+    localStorage.setItem('theme', theme);
+}
